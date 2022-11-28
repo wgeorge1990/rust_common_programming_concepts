3b3e3bb9d759cd1a5c7afba3b3b46da3ea7ef216
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-11-28 09:19:04 -0500

    Add: comments about rules of using statements in rust functions.

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -20,9 +20,15 @@ fn function_with_params(x: i32) {
 // Expressions do not include ending semicolons. If you add a semicolon to the end of an expression, 
 //   you turn it into a statement, which will then not return a value.
 
+// function that returns a value
+// becasue line 26 outputs a value it is considered a statement and there for
+// requires no semicolon after the 5 and also requires the return type to be
+// identified ahead of time using -> _type_ inbetween the params holder and the fn block {}
 fn five() -> i32 {
     5
 }
+
+
  fn print_five() {
     let x = five();
     println!("The value of x is: {}", x);
