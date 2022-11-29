9a780bedcfd40ddd65fa454db1deb9c20f0cec37
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-11-29 13:53:48 -0500

    double free error

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,10 +1,34 @@
 fn main() {
-    // let s = "hello";
     let mut s = String::from("hello");
     println!("s = {}", s);
     s.push_str(", world!");
     println!("s = {}", s);
+
+    //copied and save to the stack in two differnt places
+    let x = 5;
+    let y = x;
+
+    //consists of three parts stored on the stack:
+    // a pointer to the memory that hold the contents of the string
+    // a length(how much memory in bytes)
+    // a pointer
+    // the capacity is the total amount of memory in bytes that the string
+    // has recieved from the operating system.
+
+    let s1 = String::from("hello");
+    let s2 = s1;
+    // s1 and s2 both exist on the heap 
+    //  as a pointer to the index and value on the stack,
+    //  length,
+    //  and capacity.
+    // println!("{}, world!", s1); rust prevents user from using
+    //invalidated reference which causes a double free error which 
+    //occurs when the same memory is tried to be cleared twice.
+    println!("{}, world!", s2);
+
 }
+
+
 // the stack and the heap
 // both are parts of memory that are available
 // to your code to use at runtime however they
