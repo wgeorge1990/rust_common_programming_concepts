0eb217d53effab39da00e365280e9c3619da0425
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-11-30 09:53:40 -0500

    refactor: using slices to solve the problem in progress

--- ownership_slices/src/main.rs
+++ ownership_slices/src/main.rs
@@ -1,7 +1,11 @@
 fn main() {
     println!("Hello, world!");
-    let s: String = String::from("hello, world");
-    let length: usize = first_word(&s);
+    let s: String = String::from("hello world");
+
+    // let length: usize = first_word(&s);
+    let length: &str = first_word(&s);
+
+    println!("string length: {}", s.len());
     println!("The length of the first word is: {}", length);
     println!("{}", length);
 }
@@ -12,14 +16,19 @@ fn main() {
 // finds in that string. If the function doesn’t find a 
 // space in the string, the whole string must be one word, 
 // so the entire string should be returned.
-fn first_word(s: &String) -> usize {
+
+// fn first_word(s: &String) -> usize {
+fn first_word(s: &String) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         // searching for space in string using byte literal syntax: b' '
         if item == b' ' {
-            return i;
+            println!("item: '{}' at index {}", item, i);
+            // return &s[0..i];
+                //or
+            return &s[..i];
         }
     }
-    s.len()
+    &s[..]
 }
