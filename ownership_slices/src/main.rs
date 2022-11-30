cf90418ef7271f3d3f99927fb3c6c68a503983f3
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-11-30 09:15:07 -0500

    first_word: part one

--- ownership_slices/src/main.rs
+++ ownership_slices/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
     println!("Hello, world!");
+    let s: String = String::from("hello, world");
+    let length: usize = first_word(&s);
+    println!("The length of the first word is: {}", length);
+    println!("{}", length);
+}
+
+
+// Here’s a small programming problem: write a function 
+// that takes a string and returns the first word it 
+// finds in that string. If the function doesn’t find a 
+// space in the string, the whole string must be one word, 
+// so the entire string should be returned.
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+    s.len()
 }
