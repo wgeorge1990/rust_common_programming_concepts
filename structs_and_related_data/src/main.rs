94751bc5555444cc29dd9e0d6e2dbf1ae41e8936
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-01 13:51:09 -0500

    builder fn for struct instance will named parameters

--- structs_and_related_data/src/main.rs
+++ structs_and_related_data/src/main.rs
@@ -1,3 +1,36 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Structs and structuring related data!");
+    struct User {
+        username: String,
+        email: String,
+        sign_in_count: u64,
+        active: bool,
+    }
+
+    let mut user1 = User {
+        username: String::from("willg529"),
+        email: String::from("someexample@example.com"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    fn build_user(email: String, username: String) -> User {
+        User {
+            email: email,
+            username: username,
+            active: true,
+            sign_in_count: 1,
+        }
+    };
+
+    user1.email = String::from("example@example.com");
+
+    let mut user2 = build_user(String::from("wg@ex.com"), String::from("wg"));
+
+    println!("username: {}", user2.username);
+    println!("email : {}", user2.email);
+    println!("active: {}", user2.active);
+    println!("sign_in_count: {}", user2.sign_in_count);
 }
+
+
