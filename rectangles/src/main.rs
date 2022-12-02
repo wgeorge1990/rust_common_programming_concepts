b0d5c45fb53a3ab216ff2e362bf3504524ef2e20
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-01 21:16:13 -0500

    refactor: use deminsion tuple as parameters for area function

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,14 +1,17 @@
 fn main() {
     println!("calculate area of rectangle");
 
-    let width1 = 30;
-    let height1 = 50;
+    // let width1 = 30;
+    // let height1 = 50;
+
+    let rect1 = (30, 50);
 
     println!("The area of the rectangle is {} square pixels.",
-        area(width1, height1)
+        // area(width1, height1)
+        area(rect1)
     );
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area( deminsions: (u32, u32) ) -> u32 {
+    deminsions.0 * deminsions.1
 }
