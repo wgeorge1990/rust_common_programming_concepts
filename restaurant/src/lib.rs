18e6f1e07c72c28ea61d550ae0dbe26ccd185b1a
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-02 13:36:09 -0500

    modules vs enums: public vs private scope

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -48,8 +48,7 @@ mod back_of_house {
 
 }
 
-
-
+// public functions in crate or root scope
 pub fn eat_at_restaurant() {
     let order1 = back_of_house::Appetizer::Soup;
     let order2 = back_of_house::Appetizer::Salad;
