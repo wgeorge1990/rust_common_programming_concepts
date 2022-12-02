ae1b6906b39abb1966605c7e59c67afd28be434f
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-01 21:21:41 -0500

    refactor: use struct to represent rectangle deminsions passed into area function

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,17 +1,24 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
     println!("calculate area of rectangle");
 
     // let width1 = 30;
     // let height1 = 50;
 
-    let rect1 = (30, 50);
+    // let rect1 = (30, 50);
+
+    let rect1 = Rectangle { width: 30, height: 50 };
 
     println!("The area of the rectangle is {} square pixels.",
         // area(width1, height1)
-        area(rect1)
+        area(&rect1)
     );
 }
 
-fn area( deminsions: (u32, u32) ) -> u32 {
-    deminsions.0 * deminsions.1
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
