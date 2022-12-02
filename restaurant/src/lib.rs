d9b2114daaf721b34f50b442c27bb8c411d05531
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-02 13:35:51 -0500

    modules vs enums: public vs private scope

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -24,7 +24,44 @@ mod front_of_house {
     }
 }
 
+mod back_of_house {
+    //public scoping works the same in modules as it
+    //does in structs. ENUMs on the other hand work as a whole.
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+}
+
+
+
 pub fn eat_at_restaurant() {
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+
+    // Order breakfast in the summer with rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Then change our mind about the bread via
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+    // meal.seasonal_fruit = String::from("blueberries");
+    // above line will not work if uncommented
+
     // Absolute path
     crate::front_of_house::hosting::add_to_waitlist();
     
