73f6492bb2d9cf20cff5d3d8672eacd60f5b98b4
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-01 22:37:00 -0500

    embeded enum types for ipv4 and ipv6 ip address types

--- enums_and_pattern_matching/src/main.rs
+++ enums_and_pattern_matching/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    println!("--------------------------");
+    println!("ENUMS AND PATTERN MATCHING");
+    println!("--------------------------");
+    #[derive(Debug)]
+    struct Ipv4Addr {
+        address: String,
+    }
+    #[derive(Debug)]
+    struct Ipv6Addr {
+        address: (u32, u32, u32, u32),
+    }
+
+    #[derive(Debug)]
+    enum IpAddr {
+        V4(Ipv4Addr),
+        V6(Ipv6Addr),
+    }
+
+    let home = Ipv4Addr { address: String::from("1.27.0.0.1")};
+
+    let home_v4 = IpAddr::V4(home);
+    println!("{:?}", home_v4);
+
+
+
 }
