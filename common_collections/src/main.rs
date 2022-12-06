99a3deb0d80a7b3f8422339830e15a2c1310941e
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-06 11:41:02 -0500

    HashMap: insert key and value and loop/print key and value

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -114,5 +114,12 @@ fn iterating_over_strings() {
 
 fn storing_keys_and_values_in_hasmaps() {
     println!("storing_keys_and_values_in_hasmaps()");
-
+    use std::collections::HashMap;
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+    println!("{:?}",scores);
+    for (key, value) in scores.iter_mut() {
+        println!("{} '{}' ", key, value);
+    }
 }
