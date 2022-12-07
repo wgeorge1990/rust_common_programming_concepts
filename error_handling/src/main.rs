362b81ed206d52a090f6ca0d43c375852674ea55
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-07 13:48:43 -0500

    shortcut for propogating errors using ? operator

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -16,8 +16,12 @@ fn main() {
     //         other_error => panic!("Problem opening the file: {:?}", other_error),
     //     },
     // };
-    // handling_with_closures();
-    println!("{:?}",read_username_from_file().expect("Something went wrong").chars());
+
+    handling_with_closures();
+    
+    println!("{:?}",read_username_from_file().expect("Something went wrong"));
+    // vs using ? as shortcut for propogating errors: way shorter and cleaner method.
+    println!("{:?}",shortcut_for_propogating_errors().expect("Something went wrong"));
 }
 
 fn handling_with_closures() {
@@ -32,7 +36,7 @@ fn handling_with_closures() {
     });
 }
 
-fn read_username_from_file() -> Result<String,io::Error> {
+fn read_username_from_file() -> Result<String, io::Error> {
     let f = File::open("readusernamefromfile.txt");
 
     let mut f = match f {
@@ -46,4 +50,11 @@ fn read_username_from_file() -> Result<String,io::Error> {
         Err(e) => Err(e),
     }
 
+}
+
+fn shortcut_for_propogating_errors() -> Result< String, io::Error> {
+    let mut f = File::open("readusernamefromfile.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
 }
