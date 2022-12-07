e21a52fab05b9cde3e66541315f353df9a712081
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-07 11:56:39 -0500

    create file, check for file, read username if found in file, return chars or error handling

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,17 +1,49 @@
+use std::io;
+use std::io::Read;
 use std::fs::File;
-use std::io::ErrorKind;
+
 
 fn main() {
-    let f = File::open("hello.txt");
+    // let f = File::open("hello.txt");
 
-    let f = match f {
+    // let f = match f {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => match File::create("hello.txt") {
+    //             Ok(fc) => fc,
+    //             Err(e) => panic!("Problem creating the file: {:?}", e),
+    //         },
+    //         other_error => panic!("Problem opening the file: {:?}", other_error),
+    //     },
+    // };
+    // handling_with_closures();
+    println!("{:?}",read_username_from_file().expect("Something went wrong").chars());
+}
+
+fn handling_with_closures() {
+    let f = File::open("readusernamefromfile.txt").unwrap_or_else( |error| {
+        if error.kind() == io::ErrorKind::NotFound {
+            File::create("readusernamefromfile.txt").unwrap_or_else( |error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+}
+
+fn read_username_from_file() -> Result<String,io::Error> {
+    let f = File::open("readusernamefromfile.txt");
+
+    let mut f = match f {
         Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("Problem creating the file: {:?}", e),
-            },
-            other_error => panic!("Problem opening the file: {:?}", other_error),
-        },
+        Err(e) => return Err(e),
     };
+
+    let mut s = String::new();
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+
 }
