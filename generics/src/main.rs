d0226e7cf7a8de28aaf1ffb9ac90ea12d1a513cb
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-08 09:58:01 -0500

    largest number in vec

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Generics");
+    //Removing duplication by extracting a function
+    let number_list = vec![34, 50, 25, 100, 65];
+    let mut largest = number_list[0];
+    for number in number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+    println!("The largest number is {}", largest);
 }
