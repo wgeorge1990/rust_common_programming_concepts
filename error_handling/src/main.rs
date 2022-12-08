f58e6b43eec0e7c19e2054ef2ec504f6e2a1bf6b
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-08 09:52:10 -0500

    tail end of error handling

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,6 +1,6 @@
 use std::io;
 use std::io::Read;
-use std::fs::File;
+use std::fs;
 
 
 fn main() {
@@ -18,16 +18,20 @@ fn main() {
     // };
 
     handling_with_closures();
-    
+
     println!("{:?}",read_username_from_file().expect("Something went wrong"));
     // vs using ? as shortcut for propogating errors: way shorter and cleaner method.
     println!("{:?}",shortcut_for_propogating_errors().expect("Something went wrong"));
+    // chaining and propogating
+    println!("{:?}", chaining_and_propogating().expect("expect"));
+
+    println!("{}",chaining_reduced().expect("expect error, run backtrace to find code break"));
 }
 
 fn handling_with_closures() {
-    let f = File::open("readusernamefromfile.txt").unwrap_or_else( |error| {
+    let f = fs::File::open("readusernamefromfile.txt").unwrap_or_else( |error| {
         if error.kind() == io::ErrorKind::NotFound {
-            File::create("readusernamefromfile.txt").unwrap_or_else( |error| {
+            fs::File::create("readusernamefromfile.txt").unwrap_or_else( |error| {
                 panic!("Problem creating the file: {:?}", error);
             })
         } else {
@@ -37,7 +41,7 @@ fn handling_with_closures() {
 }
 
 fn read_username_from_file() -> Result<String, io::Error> {
-    let f = File::open("readusernamefromfile.txt");
+    let f = fs::File::open("readusernamefromfile.txt");
 
     let mut f = match f {
         Ok(file) => file,
@@ -53,8 +57,18 @@ fn read_username_from_file() -> Result<String, io::Error> {
 }
 
 fn shortcut_for_propogating_errors() -> Result< String, io::Error> {
-    let mut f = File::open("readusernamefromfile.txt")?;
+    let mut f = fs::File::open("readusernamefromfile.txt")?;
     let mut s = String::new();
     f.read_to_string(&mut s)?;
     Ok(s)
+}
+
+fn chaining_and_propogating() -> Result<String, io::Error> {
+    let mut s = String::new();
+    fs::File::open("readusernamefromfile.txt")?.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn chaining_reduced() -> Result<String, io::Error> {
+    fs::read_to_string("readusernamefromfile.txt")
 }
