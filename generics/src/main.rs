db05e4d9baea2517f91984df1770c1b0eafe287d
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-08 21:53:24 -0500

    using dublicate logic: prior to refactor and application of generics and lifetimes

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -9,4 +9,14 @@ fn main() {
         }
     }
     println!("The largest number is {}", largest);
+
+    //find the largest number in the next vector
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let mut largest = number_list[0];
+    for number in number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+    println!("The largest number in the next vector is {}", largest);
 }
