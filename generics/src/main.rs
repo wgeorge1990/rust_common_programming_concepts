c35dc4c540de73501b86495e63b18f6b214c4c4c
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-08 22:22:36 -0500

    Update fn largest to generic to take in parameter list of a slice of type T and return value of type T:

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,4 +1,4 @@
-fn largest(list: &[i32]) -> i32 {
+fn largest<T>(list: &[T]) -> T {
     let mut largest = list[0];
 
     for &item in list.iter() {
@@ -6,31 +6,48 @@ fn largest(list: &[i32]) -> i32 {
             largest = item;
         }
     }
-    
     largest
 }
+
+fn largest_char(list: &[char]) -> char {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}   
+
+//---REFACTORING STEPS-----:
+//   1. Identify duplicate code. 
+//   2. Extract the duplicate code into the body of the function 
+//       and specify the inputs and return values of that code in the function signature. 
+//   3. Update the two instances of duplicated code to call the function instead.
+//source => Klabnik, Steve; Nichols, Carol. The Rust Programming Language (Covers Rust 2018) (p. 174). No Starch Press. Kindle Edition. 
 
 fn main() {
     println!("Generics");
     //Removing duplication by extracting a function
     let number_list = vec![34, 50, 25, 100, 65];
-    let high_number = largest(&number_list);
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+    
+    // code block replaced by initial recactor
     // let mut largest = number_list[0];
     // for number in number_list {
     //     if number > largest {
     //         largest = number;
     //     }
     // }
-    println!("The largest number is {}", high_number);
 
     //find the largest number in the next vector
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let high_number = largest(&number_list);
-    // let mut largest = number_list[0];
-    // for number in number_list {
-    //     if number > largest {
-    //         largest = number;
-    //     }
-    // }
-    println!("The largest number in the next vector is {}", high_number);
+    let result = largest(&number_list);
+    println!("The largest number in the next vector is {}", result);
+
+    let char_list = vec!['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+    let result = largest_char(&char_list);
+    println!("The largest char is {}", result);
 }
