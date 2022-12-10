4220d635f47a5d2bff723d90f4c98a004e6e3c8a
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-09 19:54:24 -0500

    adding lifetime to signature and then applying to parameters in longest function

--- validating_refs_with_lifetimes/src/main.rs
+++ validating_refs_with_lifetimes/src/main.rs
@@ -1,4 +1,9 @@
-fn longest(x: &str, y: &str) -> &str {
+//examples of lifetime annotations
+// &i32 a reference
+// &'a i32 a reference with an explicit lifetime
+// &'a mut i32 a mutable reference with an explicit lifetime
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
     } else if y.len() > x.len() {
