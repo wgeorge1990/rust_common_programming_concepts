d4af3d970a4f8f3beabc3039e9f30d3c1d75565d
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-11 02:00:25 -0500

    add unit test examples: should_paanic macro and assert_eq! macro

--- libraries/adder/src/lib.rs
+++ libraries/adder/src/lib.rs
@@ -18,9 +18,39 @@ impl Rectangle {
     }
 }
 
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1|| value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}", value);
+        }
+
+        Guess {
+            value
+        }
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    #[should_panic]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+    #[test]
+    fn guess_acceptable() {
+        Guess::new(99);
+    }
 
     #[test]
     fn exploration() {
@@ -57,7 +87,17 @@ mod tests {
         };
         assert!(!smaller.can_hold(&larger));
     }
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("William");
+        assert!(
+            result.contains("William"),
+            "Greeting did not contain name, value was `{}`", result
+        );
+    }
 }
 //assert! macro evaluates to a boolean, if true, assert! does nothing and test passes.
 //Benchmark test documentation
 //https://doc.rust-lang.org/nightly/unstable-book/library-features/test.html
+//Other programming languages: actual and expected
+//rust: left and right
