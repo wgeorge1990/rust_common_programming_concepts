ab1353f828b0c5c80e12eedd7c32ceba92dcc49b
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-10 19:24:17 -0500

    add informational commenting on tests in rust

---
+++ writing_automated_tests/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("Writing Automated Tests, Chapter 11");
+    // How to write tests 
+    // Tests are rust functions that verify that the non-test code
+    // is functioning in the expected manner. The bodies of test functions
+    // typically perform these three actions:
+    // 1. Set up any needed data or state.
+    // 2. Run the code you want to test.
+    // 3. Assert the result are what you expect.
+    //    - the test attribute
+    //    - a few macros
+    //    - and the should_panic attribute
+    // a. add #[test] attribute above the fn test() {};
+    // b. $ cargo test => builds a test runner binary that runs the annotated #[test] functions.
+    // c. reports pass or fail for each defined test.
+
+    
+}
