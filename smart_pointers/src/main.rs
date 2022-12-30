0c20ef84dcde00cea414acc0ab4371784cf41dba
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-29 22:24:08 -0500

    Add: enum List with Cons which is recursive and needs a smart pointer to compile

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -9,8 +9,30 @@
 //! - Box<T> for allocating values on the heap 
 //! - Rc<T>, a reference counting type that enables multiple ownership 
 //! - Ref<T> and RefMut<T>, accessed through RefCell<T>, a type that enforces the borrowing rules at runtime instead of compile time
+//! - Review chapter 4 for difference between the stack and the heap!
+//! 
+use crate::List::{Cons, Nil};
+
+
+enum List {
+    Cons(i32,List),
+    Nil,
+}
+
+enum Message {
+    Quit,
+    Move{ x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
 
 fn main() {
-    println!("Smart Pointers");
+    println!("Use a box to store an i32 value on the heap");
+    let b = Box::new(5);
+    println!("b = {}", b);
+
+    let list = Box::new(Cons(1, (Cons(2, Cons(3, Nil)))));
+
+   
 
 }
