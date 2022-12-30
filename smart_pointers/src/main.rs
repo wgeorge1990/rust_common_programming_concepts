41c05282fb2d814de7a3fa7dd910044c70680e9e
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-29 21:44:00 -0500

    Add: doc of smart pointer summary.

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,5 +1,12 @@
 //! # SMART POINTERS
+//! - Smart pointers are usually implemented using structs.
+//! The characteritic that distinguishes a smart pointer from an 
+//! ordinary struct is that smart pointers implement Deref and Drop traits.
+//! Deref - allows it to behave like a ref so you can use them to work with both.
+//! Drop - allow you to customize the code that is run when an instance 
+//! of the smart pointer goes out of scope.
+//! 
 fn main() {
     println!("Smart Pointers");
-    
+
 }
