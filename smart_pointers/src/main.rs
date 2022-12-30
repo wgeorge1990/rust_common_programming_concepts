12b47873f11586f68b36670111113941d0f100c0
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-29 22:28:02 -0500

    Add: Box<List> smartpointer for recursive behavior

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -13,9 +13,9 @@
 //! 
 use crate::List::{Cons, Nil};
 
-
+#[derive(Debug)]
 enum List {
-    Cons(i32,List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -31,7 +31,12 @@ fn main() {
     let b = Box::new(5);
     println!("b = {}", b);
 
-    let list = Box::new(Cons(1, (Cons(2, Cons(3, Nil)))));
+    println!("use box to represent recursive programming");
+    let list = Cons(1, 
+        Box::new(Cons(2,
+            Box::new(Cons(3,
+                Box::new( Nil))))));
+    println!("{:?}", list);
 
    
 
