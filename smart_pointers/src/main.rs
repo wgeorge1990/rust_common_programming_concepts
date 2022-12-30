6ccd465ea469a60e3a295b1e03d8d07211ac2dce
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-29 21:47:40 -0500

    Add: doc - most common smart pointers in standard library and their paterns

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -5,7 +5,11 @@
 //! Deref - allows it to behave like a ref so you can use them to work with both.
 //! Drop - allow you to customize the code that is run when an instance 
 //! of the smart pointer goes out of scope.
-//! 
+//! ## MOST COMMON SMART POINTER IN STD LIB.
+//! - Box<T> for allocating values on the heap 
+//! - Rc<T>, a reference counting type that enables multiple ownership 
+//! - Ref<T> and RefMut<T>, accessed through RefCell<T>, a type that enforces the borrowing rules at runtime instead of compile time
+
 fn main() {
     println!("Smart Pointers");
 
