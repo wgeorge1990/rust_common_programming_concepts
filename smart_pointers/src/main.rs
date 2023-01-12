3c1dfe6b9bfb637cb8b17a0ec21b82ddea7c8ebf
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2023-01-12 14:39:20 -0500

    add: use deference operator to follow a reference to an i32 value

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,16 +1,16 @@
 //! # SMART POINTERS
 //! - Smart pointers are usually implemented using structs.
-//! The characteritic that distinguishes a smart pointer from an 
+//! The characteritic that distinguishes a smart pointer from an
 //! ordinary struct is that smart pointers implement Deref and Drop traits.
 //! Deref - allows it to behave like a ref so you can use them to work with both.
-//! Drop - allow you to customize the code that is run when an instance 
+//! Drop - allow you to customize the code that is run when an instance
 //! of the smart pointer goes out of scope.
 //! ## MOST COMMON SMART POINTER IN STD LIB.
-//! - Box<T> for allocating values on the heap 
-//! - Rc<T>, a reference counting type that enables multiple ownership 
+//! - Box<T> for allocating values on the heap
+//! - Rc<T>, a reference counting type that enables multiple ownership
 //! - Ref<T> and RefMut<T>, accessed through RefCell<T>, a type that enforces the borrowing rules at runtime instead of compile time
 //! - Review chapter 4 for difference between the stack and the heap!
-//! 
+//!
 use crate::List::{Cons, Nil};
 
 #[derive(Debug)]
@@ -21,7 +21,7 @@ enum List {
 
 enum Message {
     Quit,
-    Move{ x: i32, y: i32 },
+    Move { x: i32, y: i32 },
     Write(String),
     ChangeColor(i32, i32, i32),
 }
@@ -32,12 +32,10 @@ fn main() {
     println!("b = {}", b);
 
     println!("use box to represent recursive programming");
-    let list = Cons(1, 
-        Box::new(Cons(2,
-            Box::new(Cons(3,
-                Box::new( Nil))))));
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
     println!("{:?}", list);
 
-   
-
+    let x = 5;
+    let y = &x;
+    println!("{}", *y);
 }
