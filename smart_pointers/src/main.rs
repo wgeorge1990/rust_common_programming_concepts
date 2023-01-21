55df03fd6271ac1c1276af301645fb53d2f3e6c0
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2023-01-20 19:52:27 -0500

    using refflist to keep track of how close a value is to a maximum value and warn the value is at certain levels

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -65,6 +65,41 @@ enum SharedList {
     Nil,
 }
 
+//A use case for Interior Mutability: Mock Objects
+pub trait Messenger {
+    fn send(&self, msg: &str);
+}
+
+pub struct LimitTracker<'a, T: 'a + Messenger> {
+    messenger: &'a T,
+    value: usize,
+    max: usize,
+}
+
+impl<'a, T> LimitTracker<'a, T>
+    where T: Messenger {
+        pub fn new(messenger: &T, max: usize) -> LimitTracker<T> {
+            LimitTracker { 
+                messenger, 
+                value: 0, 
+                max, 
+            }
+        }
+
+
+pub fn set_value(&mut self, value: usize) {
+    self.value = value;
+    let percentage_of_max = self.value as f64 / self.max as f64;
+    if percentage_of_max >= 1.0 {
+        self.messenger.send("Error: You are over your quota!");
+    } else if percentage_of_max >= 0.9 {
+        self.messenger.send("Urgent warning: You've used up over 90% of your quota!");
+    } else if percentage_of_max >= 0.75 {
+        self.messenger.send("Warning: You've used up over 75% of your quota!");
+    }
+}
+    }
+
 fn main() {
     println!("Use a box to store an i32 value on the heap");
     let b = Box::new(5);
