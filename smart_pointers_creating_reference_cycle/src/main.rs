ec46a8892d3c803000ba07ca4371cb3f224468c7
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2023-01-21 11:30:14 -0500

    Uncomment last line and see what it looks like to overflow the stack

--- smart_pointers_creating_reference_cycle/src/main.rs
+++ smart_pointers_creating_reference_cycle/src/main.rs
@@ -17,7 +17,34 @@ impl List {
     }
 }
 
+
+
 fn main() {
     println!("Creating A Reference Cycle");
+    
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+    
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
+    //uncomment the next line to see that we have a cycle:
+    //it will overflow the stack...
+    // println!("a next item = {:?}", a.tail());
+    //thread main has overflowed its stack 
+    //fatal runtime error: stack overflow
+    //[1] 25101 abort cargo run
+
 
 }
