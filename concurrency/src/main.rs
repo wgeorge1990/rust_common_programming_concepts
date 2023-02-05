713568be91f30573cd382a03e3e77aed897f9267
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2023-02-05 12:07:47 -0500

    Moving the join.unwrap() before the main thread will force it to run the spawned first and then the main thread

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -3,17 +3,17 @@ use std::time::Duration;
 
 fn main() {
     println!("creating thread and printing to a spawned thread");
-    thread::spawn(|| {
-        for i in 1..10 {
-            println!("hello number {} from spawned thread!", i);
-            thread::sleep(Duration::from_millis(1));
-        }
-    });
+    // thread::spawn(|| {
+    //     for i in 1..10 {
+    //         println!("hello number {} from spawned thread!", i);
+    //         thread::sleep(Duration::from_millis(1));
+    //     }
+    // });
     
-    for i in 1..5 {
-        println!("hello number {} from the main thread!", i);
-        thread::sleep(Duration::from_millis(1));
-    }
+    // for i in 1..5 {
+    //     println!("hello number {} from the main thread!", i);
+    //     thread::sleep(Duration::from_millis(1));
+    // }
     using_join();
 }
 
@@ -25,10 +25,10 @@ fn using_join() {
         }
     });
 
+    handle.join().unwrap();
+
     for i in 1..5 {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
-
-    handle.join().unwrap();
 }
