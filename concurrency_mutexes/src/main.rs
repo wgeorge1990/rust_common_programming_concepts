4505ccdad309fd735db218ed91fef921da3ad288
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2023-02-06 22:23:54 -0500

    Using Arc instead of Rc to wrap mutex to be able to share ownership across multiple threads

--- concurrency_mutexes/src/main.rs
+++ concurrency_mutexes/src/main.rs
@@ -1,25 +1,19 @@
-use std::sync::Mutex;
+use std::rc::Rc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 fn main() {
     println!("MUTEXES in concurrency");
 
-    // let m = Mutex::new(5);
-
-    // {
-    //     let mut num = m.lock().unwrap();
-    //     *num = 6;
-    // }
-
-    // println!("m = {:?}", m);
-
-    let counter = Mutex::new(0);
     let mut handles = vec![];
 
+    let counter = Arc::new(Mutex::new(0));
+
     for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+
         let handle = thread::spawn(move || {
             let mut num = counter.lock().unwrap();
-
             *num += 1;
         });
 
