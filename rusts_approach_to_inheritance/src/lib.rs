6362709c7151a658210d9b2145098e699cffa437
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2023-02-08 10:26:06 -0500

    working implementation

--- rusts_approach_to_inheritance/src/lib.rs
+++ rusts_approach_to_inheritance/src/lib.rs
@@ -1,7 +1,43 @@
-pub triat Draw {
-    fn draw(&self);
+pub trait Draw {
+     fn draw(&self);
 }
 
 pub struct Screen {
     pub components: Vec<Box<dyn Draw>>,
+}
+
+//define the method run, on the Screen struct, that calls
+//the draw method on each of its components.
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+#[derive(Debug)]
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        //code to actually draw button
+        println!("{:?}", self);
+    }
+}
+
+pub struct SelectBox {
+    pub width: u32,
+    pub height: u32,
+    pub options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        //code to draw a select box
+        // println!("{:?}", self);
+    }
 }
